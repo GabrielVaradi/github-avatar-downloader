23154eb05466136b75639ce62c397dcd74b3b5fa
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-05-06 21:23:09 +0000

    requesting every avat url motherfucker

--- download_avatars.js
+++ download_avatars.js
@@ -13,7 +13,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    let parsedBody = JSON.parse(body)
+
+      cb(err, parsedBody);
+
   });
 }
 
@@ -22,6 +25,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  for (let i = 0; i < result.length; i++){
+    console.log(result[i].avatar_url)
+  }
+
 });
 
 
+
+// request.get('https://sytantris.github.io/http-examples/future.jpg')
+//        .on('error', function (err) {
+//          throw err;
+//        })
+//        .on('response', function (response) {
+//          console.log('Response Status Code: ', response.statusCode);
+//          console.log('Response Message: ', response.statusMessage);
+//          console.log('Response Header: ', response.headers['content-type'])
+//          console.log('Downloading Image')
+//        })
+//        .pipe(fs.createWriteStream('./future.jpg'))
+
+//        .on('finish', function(){
+//         console.log("Download complete")
+//        })
